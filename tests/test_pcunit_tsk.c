7d31ef44a9e52411d4eee8bf1dfd994cd1fd213d
tacchang001 <id001@tacchang.server-queen.jp>
2015-02-07 21:07:09 +0900

    test02で起動するスレッドの優先度をそれぞれ、SCHED_OTHER, SCHED_FIFO, SCHED_RRのポリシーにした。

--- tests/test_pcunit_tsk.c
+++ tests/test_pcunit_tsk.c
@@ -14,11 +14,6 @@
 
 const int TASK_ID = 100;
 
-static void test_example01(void)
-{
-    PCU_ASSERT_EQUAL(1+1, 2);
-}
-
 static void * anything(void * arg) {
 	int value = *(int*)arg;
 	fprintf(stderr, "value = %d\n", value);
@@ -35,7 +30,7 @@ static void * anything(void * arg) {
 	printf("\ndone.\n", value);
 }
 
-static void test_example02(void)
+static void test_example01(void)
 {
 	int a = 10;
 	ele_task_init_attr_t attr = {
@@ -55,11 +50,52 @@ static void test_example02(void)
 	ele_task_destroy(TASK_ID);
 }
 
+static void test_example02(void)
+{
+	int a = 10;
+	ele_task_init_attr_t attr = {
+		.id = TASK_ID,
+		.schedpolicy = SCHED_FIFO, // root authority is necessary
+		.schedparam = 1,
+		.entry = anything,
+		.arg = &a
+	};
+
+	if (ele_task_create(attr) != ELE_SUCCESS) {
+		fprintf(stderr, "ele_tsk_create error\n");
+	}
+
+	sleep(1);
+
+	ele_task_destroy(TASK_ID);
+}
+
+static void test_example03(void)
+{
+	int a = 10;
+	ele_task_init_attr_t attr = {
+		.id = TASK_ID,
+		.schedpolicy = SCHED_RR, // root authority is necessary
+		.schedparam = 1,
+		.entry = anything,
+		.arg = &a
+	};
+
+	if (ele_task_create(attr) != ELE_SUCCESS) {
+		fprintf(stderr, "ele_tsk_create error\n");
+	}
+
+	sleep(1);
+
+	ele_task_destroy(TASK_ID);
+}
+
 PCU_Suite *TaskTest_suite(void)
 {
 	static PCU_Test tests[] = {
-		{ "1st test", test_example01 },
-		{ "2nd test", test_example02 },
+		{ "SCHED_OTHER", test_example01 },
+		{ "SCHED_FIFO", test_example02 },
+		{ "SCHED_RR", test_example03 },
 	};
 	static PCU_Suite suite = {
 		"ExampleTest", tests, sizeof tests / sizeof tests[0] };
